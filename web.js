6f409115b51a3c40b7798fe1e4f857fc6f32f625
Valerii Hiora <valerii.hiora@gmail.com>
2013-07-05 14:45:19 +0300

    Serving from static index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var text = fs.readFileSync("index.html").toString('utf-8');
+    response.send(text);
 });
 
 var port = process.env.PORT || 5000;
